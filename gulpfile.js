8b9bdd29a84d7e1fa1692924ef2d6750ae56138e
David Clark <David.Clark@chorus.co>
2017-05-31 10:31:11 +0100

    coveralls added

--- gulpfile.js
+++ gulpfile.js
@@ -22,5 +22,19 @@ gulp.task('default', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['default']);
+    gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['build-test']);
+});
+
+gulp.task('build-test', function () {
+    var tsResult = gulp.src(['src/**/*.ts', 'test/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'build.js'
+        }));
+
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'))
+        .pipe(jasmine());
 });
