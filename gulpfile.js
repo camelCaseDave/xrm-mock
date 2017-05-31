4bc18cbcfd63837dcd0642d855da9281234ea607
David Clark <David.Clark@chorus.co>
2017-05-31 10:21:10 +0100

    istanbul coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@ var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     sourcemaps = require('gulp-sourcemaps'),
     jasmine = require('gulp-jasmine'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    istanbul = require('gulp-istanbul');
 
 gulp.task('default', function () {
-    var tsResult = gulp.src(['src/**/*.ts','test/**/*.ts'])
+    var tsResult = gulp.src(['src/**/*.ts', 'test/**/*.ts'])
         .pipe(sourcemaps.init())
         .pipe(ts({
             noImplicitAny: true,
             outFile: 'build.js'
-        }));        
+        }));
 
-        return tsResult.js
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('./'))
-            .pipe(jasmine());
+    return tsResult.js
+        .pipe(istanbul())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'))
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports());
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['src/**/*.ts','test/**/*.ts'], ['default']);
+gulp.task('watch', function () {
+    gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['default']);
 });
