d60156e3dc1f3cb6ebf20ba59a6d4f14ff8c2ec7
David Clark <David.Clark@chorus.co>
2018-05-22 10:37:08 +0100

    examples of how to stub Web API calls using SinonJS

--- wallaby.js
+++ wallaby.js
@@ -1,12 +1,14 @@
 module.exports = function () {
     return {
         files: [
+            'sinon/lib/sinon.js',
             'tsconfig.json',
             'src/**/*.ts'
         ],
         tests: [
             'test/**/*.ts'
         ],
+        debug: true,
         env: {
             type: 'node',
             runner: 'node'
